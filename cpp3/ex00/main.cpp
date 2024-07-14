0c7fa11ddc4cc68a4e767bf63eaff57588437ed1
Alexandre Ferreira Da Silva <alexafer@fu-r9-p8.s19.be>
2024-07-14 15:22:03 +0200

    update

---
+++ cpp3/ex00/main.cpp
@@ -0,0 +1,77 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alexafer <alexafer@student.s19.be>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/14 14:43:26 by alexafer          #+#    #+#             */
+/*   Updated: 2024/07/14 15:21:34 by alexafer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ClapTrap.hpp"
+
+int	main(void)
+{
+	ClapTrap Hey("Hey");
+	ClapTrap Noo("Noo!");
+	
+	Hey.attack("Noo!");
+		
+	Noo.takeDamage(3);
+	Hey.attack("Noo!");
+	Noo.takeDamage(3);
+	Hey.attack("Noo!");
+	Noo.takeDamage(3);
+	Hey.attack("Noo!");
+	Noo.takeDamage(3);
+	Hey.attack("Noo!");
+	Noo.takeDamage(3);
+	Hey.attack("Noo!");
+	Noo.takeDamage(3);
+	Hey.attack("Noo!");
+	Noo.takeDamage(3);
+	Hey.attack("Noo!");
+	Noo.takeDamage(3);
+	Hey.attack("Noo!");
+	Hey.attack("Noo!");
+	Hey.attack("Noo!");
+	Hey.attack("Noo!");
+	Hey.attack("Noo!");
+	Hey.attack("Noo!");
+	Hey.attack("Noo!");
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	Noo.beRepaired(15);
+	return (0);
+}
